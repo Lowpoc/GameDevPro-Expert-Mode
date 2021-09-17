4ff53a73b2fc258dd7d1701fcf7f4fd39596997f
Marcus Vinicius Santana Silva <marcus.silva@xpi.com.br>
2021-09-17 10:35:37 -0300

    🆕 feat: Desafio Pedra, Papel e Tesoura da SkyNet.

---
+++ desafios/fase01/PedraPapelETesouraSkyNet/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace PedraPapelETesouraSkyNet
+{
+    class Program
+    {
+        public enum Action
+        {
+            Rock,
+            Paper,
+            Scissor,
+        }
+        
+        static void Main(string[] args)
+        {
+            PrintAndWait("Bem vindo ao Rock Paper Scissor com uma IA!");
+            PrintAndWait("Esse é o meu território, então você não tem chance.");
+            PrintAndWait("(é serio, eu que fiz esse jogo)");
+            PrintAndWait("");
+            PrintAndWait("---------------------------------------");
+
+            var action = ReadAction();
+            
+            IAVictoryAlways(action);
+        }
+
+        public static void PrintAndWait(string message)
+        {
+            Console.WriteLine(message);
+            Console.ReadKey();
+        }
+
+        public static Action ReadAction()
+        {
+            var isActionValid = false;
+            var value = default(Action);
+
+            while (!isActionValid)
+            {
+                Console.Write("Escolha Rock, Paper ou Scissor --> ");
+                var choice = Console.ReadLine()?.Trim();
+
+                isActionValid = Enum.TryParse<Action>(choice, out value);
+            }
+
+            return value;
+        }
+
+        private static void IAVictoryAlways(Action action)
+        {
+            switch (action)
+            {
+                case Action.Paper:
+                    VictoryMessageIA(Action.Scissor);
+                    break;
+                    
+                case Action.Rock:
+                    VictoryMessageIA(Action.Paper);
+                    break;
+                    
+                case Action.Scissor:
+                    VictoryMessageIA(Action.Rock);
+                    break;
+                default:
+                    Console.Write("Essa opção não existe hahaha {0}", action);
+                    break;
+            };
+        }
+
+        private static void VictoryMessageIA(Action action)
+        {
+            Console.Write("Eu escolhi {0}. Você perdeu!", action);
+            Console.ReadKey();
+        }
+    }
+}
