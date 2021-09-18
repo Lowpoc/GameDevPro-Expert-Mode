13e7ef8b180bf8918ee95b40489e3f9c6e9f3370
Marcus Vinicius Santana Silva <lowpoc.developer@gmail.com>
2021-09-18 11:21:13 -0300

    Add classes to respect SRP. (#3)

---
+++ desafios/fase01/LojaDeItens/Player.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LojaDeItens
+{
+    public class Player
+    {
+        public int Coin { get; private set; }
+        public IList<Item> Bag { get; }
+
+        public Player(int coin)
+        {
+            this.Coin = coin;
+            Bag = new List<Item>();
+        }
+
+        public bool BuyItem(Item item)
+        {
+            if (item.Coin > this.Coin) return false;
+
+            this.Coin -= item.Coin;
+            
+            Console.WriteLine("Você comprou {0} por {1} coin! você tem {2}",
+                    item.Name,
+                    item.Coin,
+                    this.Coin);
+            
+            Bag.Add(item);
+            return true;
+        }
+    }
+}
--- desafios/fase01/LojaDeItens/Program.cs
+++ desafios/fase01/LojaDeItens/Program.cs
@@ -1,118 +1,12 @@
-﻿using System;
-
-namespace LojaDeItens
+﻿namespace LojaDeItens
 {
     class Program
     {
-        private static Item[] _items = new[]
-        {
-            new Item { Name  = "Pão de Queijo", Coin = 1 },
-            new Item { Name  = "Acarajé", Coin = 3 },
-            new Item { Name  = "Feijoada", Coin = 2 } 
-        };
-
-        private static Item[] _bag = Array.Empty<Item>();
-        private static int coins = 5;
-        
         static void Main(string[] args)
         {
-            PrintAndWait("Bem vindo a nossa loja!");
-            PrintAndWait("Eu vejo que você tem muitos coins com você.");
-            PrintAndWait("Humm... você quer dar uma olhada no nosso inventário? Sim ou Sim?");
-            Separator();
-            PrintItens();
-            BuyItemsUntilFinish();
-            PrintBag();
-        }
-
-        private static void PrintAndWait(string message)
-        {
-            Console.WriteLine(message);
-            Console.ReadKey();
-        }
-
-        private static void Separator()
-        {
-            Console.WriteLine("");
-            Console.WriteLine("---------------------Loja de ITEMS------------------------");
-            Console.WriteLine("");
-        } 
-        
-        private static void PrintItens()
-        {
-            Console.WriteLine("Essas são as nossas opções:");
-            for (int i = 0; i < _items.Length; i++)
-            {
-                Console.WriteLine("{0}: {1} {2}", i+1, _items[i].Name, _items[i].Coin);
-            }
-        }
-        
-        private static void PrintBag()
-        {
-            Console.WriteLine("Você não tem mais dinheiro! Esses são seus items:");
-            for (int i = 0; i < _bag.Length; i++)
-            {
-                Console.WriteLine("{0}: {1} {2}", i+1, _bag[i].Name, _bag[i].Coin);
-            }
-
-            Console.ReadKey();
-        }
-        
-        private static void BuyItemsUntilFinish()
-        {
-            bool valideInput;
-            var contador = 0;
-            Array.Resize(ref _bag, _items.Length);
-            
-            while (coins > 0)
-            {
-                valideInput = false;
-                while (!valideInput)
-                {
-                    Console.Write("Digite o número do item que você quer comprar -->");
-                    valideInput = ReadInput(contador);
-                }
-                contador++;
-            }
-        }
-
-        private static bool ReadInput(int contador)
-        {
-            var x = Console.ReadLine()?.Trim();
-
-            int.TryParse(x, out var index);
-
-            index--;
-            
-            if (index < 0 || index >= _items.Length)
-            {
-                Console.WriteLine("Eu não conheco esse item. E Você  não sai daqui até comprar!");
-                return false;
-            }
-            
-            var item = _items[index];
-
-            if (item.Coin > coins)
-            {
-                Console.WriteLine("Você não tem coins suficiente {0}", coins);
-                return false;
-            }
-
-            if (contador >= _bag.Length)
-            {
-                var newlength = contador + 1;
-                Array.Resize(ref _bag, newlength);
-            }
-
-            Console.WriteLine("Você comprou {0} por {1} coin! você tem {2}",
-                item.Name,
-                item.Coin,
-                coins - item.Coin);
-            
-            _bag[contador] = item;
-            coins -= item.Coin;
-            
-            return true;
+            var player = new Player(5);
+            var merchant = new Merchant(player);
+            merchant.OpenStore();
         }
     }
 }
